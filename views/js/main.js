2bbb901b9f67709d077f2c0a948c5c83a5ace502
wanyanxx <33455930+wanyanxx@users.noreply.github.com>
2017-11-19 16:19:48 +0800

    修改doucument.querySelectorAll

--- views/js/main.js
+++ views/js/main.js
@@ -487,12 +487,11 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.getElementById('mover');
+  var items = document.querySelectorAll('.mover');
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   var phase = Math.sin((scrollTop / 1250) );
   for (var i = 0; i < items.length; i++) {
     // document.body.scrollTop is no longer supported in Chrome.
-
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
